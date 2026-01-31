2c0359abaaad55c0f0a37999d36be64c39822144
elham <fallah.elham@example.com>
2026-01-31 17:45:13 +0100

    S3+IAM+SNS3

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -102,16 +102,16 @@ resource "aws_iam_role" "github_actions_oidc" {
           Federated = aws_iam_openid_connect_provider.github.arn
         }
         Action = "sts:AssumeRoleWithWebIdentity"
-        "Condition": {
-           "StringEquals": {
-              "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
-            },
-           "StringLike": {
-             "token.actions.githubusercontent.com:sub": [
-             "repo:ElhamFallah23/capstone-amazon-review-analytics:ref:refs/heads/main",
-             "repo:ElhamFallah23/capstone-amazon-review-analytics:pull_request",             
-             "repo:ElhamFallah23/capstone-amazon-review-analytics:ref:refs/heads/*",
-             "repo:ElhamFallah23/capstone-amazon-review-analytics:environment:dev-approve"
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          },
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:ElhamFallah23/capstone-amazon-review-analytics:ref:refs/heads/main",
+              "repo:ElhamFallah23/capstone-amazon-review-analytics:pull_request",
+              "repo:ElhamFallah23/capstone-amazon-review-analytics:ref:refs/heads/*",
+              "repo:ElhamFallah23/capstone-amazon-review-analytics:environment:dev-approve"
             ]
           }
         }
