ecae26e1559c8d3dbcae41c14fafa1f73eb0ab96
elham <fallah.elham@example.com>
2026-02-05 12:58:45 +0100

    add resource snowflake

--- infra/variables.tf
+++ infra/variables.tf
@@ -47,7 +47,7 @@ variable "snowflake_region" {
   type        = string
 }
 
-variable "SNOWFLAKE_PRIVATE_KEY" {
+variable "snowflake_private_key" {
   description = "Path to RSA private key used for JWT authentication"
   type        = string
   sensitive   = true
