b2fe66e7efd764a48ebf8f852fbf0a5b36ddc54d
elham <fallah.elham@example.com>
2026-02-10 03:10:14 +0100

    syncronization and add snowflake objects

---
+++ dbt/target/run/capstone_amazon_analytics/models/staging/stg_meta.sql
@@ -0,0 +1,30 @@
+
+  create or replace   view AMAZON_REVIEW_ANALYTICS_DEV.STAGE_STAGE.stg_meta
+  
+  
+  
+  
+  as (
+    with source as (
+
+select *
+from AMAZON_REVIEW_ANALYTICS_DEV.RAW.CAPSTONE_AMAZON_META_RAW_TABLE
+
+),
+
+renamed as (
+
+select
+product_id as product_id,
+title as product_title,
+brand as brand,
+category as category,
+cast(price as float) as price
+from source
+
+)
+
+select *
+from renamed
+  );
+
